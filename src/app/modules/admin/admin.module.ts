3de6459a4745c2ef7d17ef4623874376e639b237
draganb24 <nemanjabjelica45@gmail.com>
2024-03-31 20:25:46 +0200

    Post car page

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { PostCarComponent } from './components/post-car/post-car.component';
+import { NgZorroImportsModule } from 'src/app/NgZorroImportsModule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    PostCarComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgZorroImportsModule
   ]
 })
 export class AdminModule { }
